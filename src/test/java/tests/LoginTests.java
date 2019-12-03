7ccbc1198f32b219a69426ccab9127f8cf30d31f
Averthel <thecojove@gmail.com>
2019-12-03 19:09:07 +0100

    @Parameters practise

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import driver.DriverUtils;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import page.object.LoginPage;
 
@@ -10,18 +11,19 @@ import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends TestBase {
 
+    @Parameters({"username", "password", "expectedWarning"})
     @Test
-    public void loginUsingBadPassword(){
+    public void asUserLoginUsingIncorrectCredentials(String username, String password, String expectedWarning){
         DriverUtils.navigateToPage(APPLICATION_URL);
         LoginPage loginPage = new LoginPage();
         loginPage
-                .typeIntoUsernameField("tomsmith")
-                .typeIntoPasswordField("inocorrctPassword")
+                .typeIntoUsernameField(username)
+                .typeIntoPasswordField(password)
                 .clickLoginButton();
 
         String message = loginPage.getFlashMessage();
 
-        assertTrue(message.contains("Your password is invalid!"));
+        assertTrue(message.contains(expectedWarning));
 
     }
 }
